df2504333ae111fb1a3e411f9a9c15b13d051a33
Sx3K <sasisekhar4817@gmail.com>
2023-02-17 16:39:31 +0530

    UDP Stable

--- main/main.c
+++ main/main.c
@@ -16,12 +16,21 @@
 #include <math.h>
 #include "esp_dsp.h"
 #include "WiFiHelper.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
 
 i2s_chan_handle_t rx_handle;
 
 QueueHandle_t buffQueue;
 
 #define N_SAMPLES 2048
+
+#define HOST_IP_ADDR "192.168.137.1"
+#define PORT 3333
+
+static const char *TAG = "UDP";
 
 void LPF(int16_t*);
 
@@ -122,6 +131,44 @@ void app_main(void) {
     initNVS();
 
     WiFiConnectHelper();
+
+    int addr_family = 0;
+    int ip_protocol = 0;
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+
+    int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+    }
+
+    // Set timeout
+    // struct timeval timeout;
+    // timeout.tv_sec = 10;
+    // timeout.tv_usec = 0;
+    // setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+
+    ESP_LOGI(TAG, "Socket created");
+
+    float *count_1 = (float*) calloc(1024, sizeof(float));;
+
+    for(int i = 0; i < 1024; i++) {
+        count_1[i] = 3.14;
+    }
+
+    while(1) {
+        int err = sendto(sock, count_1, 4096, 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            break;
+        }
+        
+        vTaskDelay(512 / portTICK_PERIOD_MS);
+    }
 
     i2s_chan_config_t rx_chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
     i2s_new_channel(&rx_chan_cfg, NULL, &rx_handle);
